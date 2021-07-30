985a39e2307cc054ce720e39f566c7cb98561ba0
zsh <a857681664@gmail.com>
2021-07-30 19:51:42 +0800

    improve'

--- src/main/java/com/zshnb/patterndesign/builder/generator/Entity.java
+++ src/main/java/com/zshnb/patterndesign/builder/generator/Entity.java
@@ -3,6 +3,7 @@ package com.zshnb.patterndesign.builder.generator;
 import java.util.List;
 
 public class Entity {
+    private String packageName;
     private List<Field> fields;
 
     public List<Field> getFields() {
@@ -11,5 +12,13 @@ public class Entity {
 
     public void setFields(List<Field> fields) {
         this.fields = fields;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
     }
 }
--- src/main/java/com/zshnb/patterndesign/builder/generator/Generator.java
+++ src/main/java/com/zshnb/patterndesign/builder/generator/Generator.java
@@ -3,15 +3,14 @@ package com.zshnb.patterndesign.builder.generator;
 import com.google.gson.Gson;
 import com.squareup.javapoet.*;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.ClassUtils;
 import org.apache.commons.text.WordUtils;
 
 import javax.lang.model.element.Modifier;
 import java.io.File;
-import java.io.FileFilter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -19,11 +18,16 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Generator {
-    public static Map<String, FieldTypeFormatAndValue> fieldInitializeValue = new HashMap<>();
+    private static Map<String, FieldTypeFormatAndValue> fieldNameWithInitializeValue = new HashMap<>();
+    private static Map<String, TypeName> fieldTypeNameWithTypeName = new HashMap<>();
     static {
-        fieldInitializeValue.put("String", new FieldTypeFormatAndValue("$S", ""));
-        fieldInitializeValue.put("int", new FieldTypeFormatAndValue("$L", 0));
-        fieldInitializeValue.put("Integer", new FieldTypeFormatAndValue("$L", 0));
+        fieldNameWithInitializeValue.put("String", new FieldTypeFormatAndValue("$S", ""));
+        fieldNameWithInitializeValue.put("int", new FieldTypeFormatAndValue("$L", 0));
+        fieldNameWithInitializeValue.put("Integer", new FieldTypeFormatAndValue("$L", 0));
+
+        fieldTypeNameWithTypeName.put("int", TypeName.INT);
+        fieldTypeNameWithTypeName.put("Integer", TypeName.INT);
+        fieldTypeNameWithTypeName.put("String", TypeName.get(String.class));
     }
 
     public static void main(String[] args) {
@@ -32,18 +36,22 @@ public class Generator {
         List<File> jsonFiles = Arrays.stream(new File(url.getFile()).listFiles(pathname -> pathname.getName().endsWith(".json"))).collect(Collectors.toList());
         jsonFiles.forEach(it -> {
             System.out.println(it.getName());
-            generator.generate(it);
+            try {
+                generator.generate(it);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         });
     }
 
-    public void generate(File file) {
-
+    public void generate(File file) throws IOException {
         Gson gson = new Gson();
-        gson.fromJson()
-        String entityName = file.getName().substring(0, file.getName().indexOf("."));
-        String entityObjectLiteral = WordUtils.uncapitalize(entityName);
+        Entity entity = gson.fromJson(FileUtils.readFileToString(file, StandardCharsets.UTF_8), Entity.class);
+        String entityClassName = WordUtils.capitalize(file.getName().substring(0, file.getName().indexOf(".")));
+        String entityObjectLiteral = WordUtils.uncapitalize(entityClassName);
+        ClassName className = ClassName.get(entity.getPackageName(), entityClassName);
 
-        TypeSpec.Builder entityTypeBuilder = TypeSpec.classBuilder(entityName)
+        TypeSpec.Builder entityTypeBuilder = TypeSpec.classBuilder(entityClassName)
             .addModifiers(Modifier.PUBLIC);
         MethodSpec entityConstructor = MethodSpec.constructorBuilder()
             .addModifiers(Modifier.PRIVATE).build();
@@ -51,48 +59,61 @@ public class Generator {
 
         TypeSpec.Builder builderTypeBuilder = TypeSpec.classBuilder("Builder")
             .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
-            .addField(entityClass, WordUtils.uncapitalize(entityClass.getSimpleName()));
+            .addField(className, entityObjectLiteral, Modifier.PRIVATE);
         MethodSpec builderConstructor = MethodSpec.constructorBuilder()
-            .addParameter(entityClass, entityObjectLiteral)
+            .addParameter(className, entityObjectLiteral)
             .addStatement("this.$N = $N", entityObjectLiteral, entityObjectLiteral)
             .addModifiers(Modifier.PRIVATE).build();
         builderTypeBuilder.addMethod(builderConstructor);
 
-        Arrays.stream(entityClass.getDeclaredFields()).forEach(it -> {
-            FieldTypeFormatAndValue fieldTypeFormatAndValue = fieldInitializeValue.get(it.getType().getSimpleName());
-            FieldSpec fieldSpec = FieldSpec.builder(it.getType(), it.getName(), Modifier.PRIVATE)
+        entity.getFields().forEach(it -> {
+            FieldTypeFormatAndValue fieldTypeFormatAndValue = fieldNameWithInitializeValue.get(it.getType());
+            FieldSpec fieldSpec = FieldSpec.builder(fieldTypeNameWithTypeName.get(it.getType()), it.getName(), Modifier.PRIVATE)
                 .initializer(fieldTypeFormatAndValue.getFormat(), fieldTypeFormatAndValue.getValue()).build();
             entityTypeBuilder.addField(fieldSpec);
             MethodSpec setMethod = MethodSpec.methodBuilder(String.format("set%s", WordUtils.capitalize(it.getName())))
                 .addModifiers(Modifier.PUBLIC)
-                .returns(ClassName.get(entityClass.getPackageName(), "Builder"))
+                .addParameter(fieldTypeNameWithTypeName.get(it.getType()), it.getName())
+                .returns(ClassName.get("", "Builder"))
                 .addStatement("$N.$N = $N", entityObjectLiteral, it.getName(), it.getName())
                 .addStatement("return this").build();
 
             builderTypeBuilder.addMethod(setMethod);
             MethodSpec clearMethod = MethodSpec.methodBuilder(String.format("clear%s", WordUtils.capitalize(it.getName())))
                 .addModifiers(Modifier.PUBLIC)
-                .returns(ClassName.get(entityClass.getPackageName(), "Builder"))
+                .returns(ClassName.get("", "Builder"))
                 .addStatement(String.format("$N.$N = %s", fieldTypeFormatAndValue.getFormat()), entityObjectLiteral, it.getName(), fieldTypeFormatAndValue.getValue())
                 .addStatement("return this").build();
             builderTypeBuilder.addMethod(clearMethod);
 
             MethodSpec getMethod = MethodSpec.methodBuilder(String.format("get%s", WordUtils.capitalize(it.getName())))
                 .addModifiers(Modifier.PUBLIC)
-                .returns(it.getType())
+                .returns(fieldTypeNameWithTypeName.get(it.getType()))
                 .addStatement("return $N", it.getName()).build();
             entityTypeBuilder.addMethod(getMethod);
         });
         MethodSpec buildMethod = MethodSpec.methodBuilder("build")
             .addModifiers(Modifier.PUBLIC)
-            .returns(entityClass)
+            .returns(ClassName.get("", entityClassName))
             .addStatement("return $N", entityObjectLiteral).build();
         builderTypeBuilder.addMethod(buildMethod);
 
+        MethodSpec newBuilderMethod = MethodSpec.methodBuilder("newBuilder")
+            .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+            .returns(ClassName.get("", "Builder"))
+            .addStatement("return new $N(new $N())", "Builder", entityClassName).build();
+        entityTypeBuilder.addMethod(newBuilderMethod);
+
+        MethodSpec toBuilderMethod = MethodSpec.methodBuilder("toBuilder")
+            .addModifiers(Modifier.PUBLIC)
+            .returns(ClassName.get("", "Builder"))
+            .addStatement("return new $N(this)", "Builder").build();
+        entityTypeBuilder.addMethod(toBuilderMethod);
+
         entityTypeBuilder.addType(builderTypeBuilder.build());
-        JavaFile javaFile = JavaFile.builder(entityClass.getPackageName(), entityTypeBuilder.build())
+        JavaFile javaFile = JavaFile.builder(entity.getPackageName(), entityTypeBuilder.build())
             .build();
 
-        javaFile.writeTo(System.out);
+        javaFile.writeToFile(new File(String.format("%s.java", entityClassName)));
     }
 }
