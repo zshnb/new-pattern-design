0639ad1ad7d60db5b6ae9c83db3bdc6078f20502
zshnb <a857681664@gmail.com>
2021-10-28 22:28:48 +0800

    delete proxy

--- src/test/java/com/zshnb/patterndesign/builder/generator/proxy/CglibProxy.java
+++ src/test/java/com/zshnb/patterndesign/builder/generator/proxy/CglibProxy.java
@@ -2,15 +2,11 @@ package com.zshnb.patterndesign.builder.generator.proxy;
 
 import com.zshnb.patterndesign.proxy.cglib.ProxyManager;
 import com.zshnb.patterndesign.proxy.cglib.UserDao;
-import com.zshnb.patterndesign.proxy.cglib.UserDaoProxy;
-import net.sf.cglib.proxy.Enhancer;
 import org.junit.jupiter.api.Test;
-
-import java.lang.reflect.InvocationTargetException;
 
 public class CglibProxy {
     @Test
-    public void aop() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    public void aop() {
         ProxyManager proxyManager = new ProxyManager();
         UserDao userDao = (UserDao) proxyManager.getProxy(UserDao.class);
         userDao.listNames();
